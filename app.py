16a3f1cd856e9b19a6f37bc59384d12c5db05a38
Youssef <youssefosamasalah51@gmail.com>
2026-05-27 02:12:07 +0300

    This was the most difficult part of the project i had to learn SQLite databases and that tokk a lot of my time i added a button to indicate whether a task is complete or not and i also make it revert to tasked as well meaning you can swetch between the two states i added a few clikcs to the completed task Now i'll start making the site look better by adding a lot of CSS

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
 from flask import Flask , render_template, request , url_for , redirect 
+import sqlite3
 
 app = Flask(__name__)
 
-tasks = []
+def db_connection ():
+    conn = sqlite3.connect("task.db")
+    return conn
+def db_init():
+    conn = db_connection()
+    cursor = conn.cursor()
+    cursor.execute(""" CREATE TABLE IF NOT EXISTS tasks (
+                   id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   task TEXT NOT NULL ,
+                   completed INTEGER DEFAULT 0 )
+                   """)
+    conn.commit()
+    conn.close()
+    
 
 @app.route("/" , methods=["GET","POST"])
 def home():
@@ -11,16 +25,49 @@ def home():
     elif request.method == "POST":
         task = request.form.get('task')
         if task.strip():
-            tasks.append(task.strip())
+            conn = db_connection()
+            cursor = conn.cursor()
+            cursor.execute('INSERT INTO tasks (task) VALUES (?)', (task,))
+            conn.commit()
+
+    conn = db_connection()
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM tasks')
+    tasks = cursor.fetchall()
+    conn.commit()
+    conn.close()
     return render_template ('home.html',tasks=tasks)
 
 @app.route("/delete/<int:num>")
 def delete(num):
-    if num < len(tasks):
-        tasks.pop(num)
+
+    conn = db_connection()
+    cursor = conn.cursor()
+    cursor.execute('DELETE FROM tasks WHERE id = (?)', (num,))
+    conn.commit()
+    conn.close()
+    return redirect(url_for('home'))
+
+@app.route("/complete/<int:num>")
+def complete(num):
+    conn = db_connection()
+    cursor =  conn.cursor()
+    cursor.execute('UPDATE tasks SET completed = 1 WHERE id = (?)' , (num,) )
+    conn.commit()
+    conn.close()
+    return redirect(url_for('home'))
+
+@app.route("/uncomplete/<int:num>")
+def uncomplete(num):
+    conn = db_connection()
+    cursor =  conn.cursor()
+    cursor.execute('UPDATE tasks SET completed = 0 WHERE id = (?)' , (num,) )
+    conn.commit()
+    conn.close()
     return redirect(url_for('home'))
 
 
+db_init()
 if __name__ == "__main__":    
     app.run(debug=True)
 
